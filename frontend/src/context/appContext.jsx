0cfbd705ffbfd09b8d856cc74f6f8deb6788ad83
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-22 20:43:11 +0530

    added front setup with react

---
+++ frontend/src/context/appContext.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react'
+import { useState } from 'react'
+import { createContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const AppContext = createContext()
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null)
+  const [isSeller, setIsSeller] = useState(false)
+
+  const value = { navigate, user, setUser }
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>
+}
+
+export const useAppContext = () => {
+  return useContext(AppContext)
+}
