22ba52e8ef162c0f6ace7ba2fafd0e628a68b6a0
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-25 01:20:51 +0530

    fix:fix currency symbol

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -47,9 +47,11 @@ const ProductCard = ({ product }) => {
           </div>
           <div className="flex items-end justify-between mt-3">
             <p className="md:text-xl text-base font-medium text-primary">
-              {currency}${product.offerPrice}{' '}
+              {currency}
+              {product.offerPrice}{' '}
               <span className="text-gray-500/60 md:text-sm text-xs line-through">
-                ${product.price}
+                {currency}
+                {product.price}
               </span>
             </p>
             <div
--- frontend/src/context/appContext.jsx
+++ frontend/src/context/appContext.jsx
@@ -10,7 +10,7 @@ export const AppContext = createContext()
 
 export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate()
-  const currency = import.meta.VITE_CURRENCY
+  const currency = import.meta.env.VITE_CURRENCY
 
   const [user, setUser] = useState(null)
   const [isSeller, setIsSeller] = useState(false)
