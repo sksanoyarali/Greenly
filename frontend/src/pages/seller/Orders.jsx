790a59a519daf39a8c64d6ec4319865fa98ca16b
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-27 20:15:32 +0530

    adder orders page for admin

--- frontend/src/pages/seller/Orders.jsx
+++ frontend/src/pages/seller/Orders.jsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from 'react'
 import { assets, dummyOrders } from '../../assets/assets'
 import { useAppContext } from '../../context/appContext'
+import toast from 'react-hot-toast'
 
 const Orders = () => {
-  const { currency } = useAppContext()
+  const { currency, axios } = useAppContext()
 
   const [orders, setOrders] = useState([])
   const fetchOrders = async () => {
-    setOrders(dummyOrders)
+    try {
+      const { data } = await axios.get('api/order/seller')
+
+      if (data.success) {
+        setOrders(data.orders)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
   useEffect(() => {
     fetchOrders()
