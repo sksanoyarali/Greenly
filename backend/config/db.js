19f98f9f871d9840b778300516da0242d3bc6830
SK sanoyar ali <asksanoyar33@gmail.com>
2026-01-22 23:26:41 +0530

    made DB connection standard

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,15 @@ import mongoose from 'mongoose'
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/greenly`)
-    console.log('Database connected successfully')
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'greenly',
+    })
+
+    console.log('✅ MongoDB connected')
   } catch (error) {
-    console.error('Error in db connection')
+    console.error('❌ MongoDB connection failed:', error.message)
     process.exit(1)
   }
 }
+
 export default dbConnect
