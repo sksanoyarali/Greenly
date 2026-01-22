c3c2330c2d94cfedfa0220a6693889d42ac22e7d
SK sanoyar ali <asksanoyar33@gmail.com>
2026-01-23 00:01:48 +0530

    added validation constraint in userSchema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,15 +5,21 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
+      maxlength: [25, 'Name cannot exceed 25 characters'],
+      minlength: [3, 'name must be at least 3 characters'],
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
+      minlength: [6, 'Password must be at least of 6 characters'],
     },
     cartItems: {
       type: Object,
